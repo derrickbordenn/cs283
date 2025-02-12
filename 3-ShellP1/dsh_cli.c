1376c47834d962554897955ea836f0eb144d577e
db3532 <db3532@tux3.cci.drexel.edu>
2025-02-11 19:50:53 -0500

    finished a2

--- 3-ShellP1/dsh_cli.c
+++ 3-ShellP1/dsh_cli.c
@@ -46,10 +46,36 @@
  */
 int main()
 {
-    char *cmd_buff;
-    int rc = 0;
-    command_list_t clist;
+	char cmd_buff[SH_CMD_MAX];
+	int rc = 0;
+	command_list_t clist;
 
-    printf(M_NOT_IMPL);
-    exit(EXIT_NOT_IMPL);
+	while (1){
+		printf("%s", SH_PROMPT);
+
+		if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL) {
+			printf("\n");
+			break;
+		}
+
+		cmd_buff[strcspn(cmd_buff, "\n")] = '\0';
+
+		if (strlen(cmd_buff) == 0){
+			printf(CMD_WARN_NO_CMD);
+			continue;
+		}
+
+		if (strcmp(cmd_buff, EXIT_CMD) == 0){
+			break;
+		}
+
+		rc = build_cmd_list(cmd_buff, &clist);
+		if (rc == WARN_NO_CMDS) {
+			printf(CMD_WARN_NO_CMD);
+		} else if (rc == ERR_TOO_MANY_COMMANDS){
+			printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+		}
+	}
+
+	return 0;
 }
--- 3-ShellP1/dshlib.c
+++ 3-ShellP1/dshlib.c
@@ -34,6 +34,73 @@
  */
 int build_cmd_list(char *cmd_line, command_list_t *clist)
 {
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+	char *token;
+	char *saveptr;
+	int num_cmds = 0;
+
+	//set everything in the clist memory block to 0
+	memset(clist, 0, sizeof(command_list_t));
+
+
+	token = strtok_r(cmd_line, PIPE_STRING, &saveptr);
+	while (token != NULL){
+
+		//trim leading spaces
+		while (*token == SPACE_CHAR) {
+			token++;
+		}
+
+		//trim trailing spaces
+		char *end = token + strlen(token) - 1;
+		while (end > token && *end == SPACE_CHAR) {
+			*end-- = '\0';
+		}
+		if (num_cmds >= CMD_MAX) {
+			return ERR_TOO_MANY_COMMANDS;
+		}
+
+		command_t *cmd = &clist->commands[num_cmds];
+		char *argptr;
+
+		//get the executable name (first command)
+		argptr = strtok(token, " ");
+
+		if (argptr == NULL || strlen(argptr) >= EXE_MAX) {
+			return ERR_CMD_OR_ARGS_TOO_BIG;
+		}
+		strcpy(cmd->exe, argptr);
+		
+		//get the arguments (rest of commands)
+		argptr = strtok(NULL, "");
+		if (argptr != NULL && strlen(argptr) >= ARG_MAX) {
+			return ERR_CMD_OR_ARGS_TOO_BIG;
+		}
+		if (argptr) {
+			//print the arguments of the executable
+			snprintf(cmd->args, ARG_MAX, "[%s]", argptr);
+		} else {
+			cmd->args[0] = '\0';
+		}
+
+		num_cmds++;
+		token = strtok_r(NULL, PIPE_STRING, &saveptr);
+	}
+	clist->num = num_cmds;
+	
+	if (num_cmds == 0) {
+		printf(CMD_WARN_NO_CMD);
+		return WARN_NO_CMDS;
+	}
+	
+	//print and format strings according to test file
+	printf(CMD_OK_HEADER, num_cmds);
+	for (int i = 0; i < num_cmds; i++) {
+		if (strlen(clist->commands[i].args) > 0) {
+			printf("<%d> %s %s\n", i + 1, clist->commands[i].exe, clist->commands[i].args);
+		} else {
+			printf("<%d> %s\n", i + 1, clist->commands[i].exe);
+		}
+	}
+
+	return OK;
 }
